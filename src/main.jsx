1d6e257145c21b775e8146155d2d9d6d3a9abd2d
SAM <ytytyt456789@gmail.com>
2025-02-05 18:36:06 +0500

    Second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {ContextProvider} from "./components/dashboard/new-dashboard/contexts/ContextProvider.jsx"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { ContextProvider } from './components/dashboard/new-dashboard/contexts/ContextProvider.jsx';
 import { registerLicense } from '@syncfusion/ej2-base';
 
-// Registering Syncfusion<sup style="font-size:70%">&reg;</sup>  license key
-registerLicense('ORg4AjUWIQA/Gnt2XVhhQlJHfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hTH5RdkdiW3pYdXFcQWZe');
-
+// Registering Syncfusion license key from environment variable
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 createRoot(document.getElementById('root')).render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
-)
+    <ContextProvider>
+        <App />
+    </ContextProvider>
+);
